41892c47fa005048fedf664e3980c82b485d589a
Ruben Carballo <rcarballoq@gmail.com>
2022-04-04 11:49:31 +0200

    RATE PLAN
    Added some formatting to dates, and types

--- airWorkSpace/pages/center/ratePlan.vue
+++ airWorkSpace/pages/center/ratePlan.vue
@@ -1,41 +1,47 @@
 <template>
-  <v-row>
-    <v-card flat class="ma-auto">
-      <v-card-title>
-        <v-icon @click="$router.push({ path:`/center/`, query:{id: center._id}, component:'CenterPage'})">
-          mdi-chevron-left
-        </v-icon>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card flat class="ma-auto">
+        <v-card-title>
+          <v-icon @click="$router.push({ path:`/center/`, query:{id: center._id}, component:'CenterPage'})">
+            mdi-chevron-left
+          </v-icon>
 
-        Tarifas
-      </v-card-title>
-    </v-card>
-    <v-card
-      v-for="(rate, idx) in ratePlan"
-      :key="idx"
-      width="90%"
-      class="ma-auto"
-    >
-      <v-card-title>{{ ratePlan[idx].name }}</v-card-title>
-      <v-card-subtitle>{{ ratePlan[idx].appliesTo }}</v-card-subtitle>
-      <v-card-text>
-        <v-input>{{ ratePlan[idx].description }}</v-input>
-        <v-input label="Disponible desde: ">
-          {{ ratePlan[idx].fromDate }}
-        </v-input>
-        <v-input label="Disponible hasta: ">
-          {{ ratePlan[idx].toDate }}
-        </v-input>
-        <v-input label="Precio/día: ">
-          {{ ratePlan[idx].price }}€
-        </v-input>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer />
-        <v-btn @click="$router.push({ path:`/booking`, query:{id: center._id, rate: ratePlan[idx]._id}, component:'BookingPage'})">
-          Reservar
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+          Tarifas
+        </v-card-title>
+      </v-card>
+      <v-card
+        v-for="(rate, idx) in ratePlan"
+        :key="idx"
+        width="90%"
+        class="ma-auto"
+      >
+        <v-card-title>{{ ratePlan[idx].name }}</v-card-title>
+        <v-card-subtitle>{{ getType(idx) }}</v-card-subtitle>
+        <v-card-text>
+          <v-input>{{ ratePlan[idx].description }}</v-input>
+          <v-input label="Disponible desde: ">
+            <span class="ml-2">
+              {{ formatDate(ratePlan[idx].fromDate) }}
+            </span>
+          </v-input>
+          <v-input label="Disponible hasta: ">
+            <span class="ml-2">
+              {{ formatDate(ratePlan[idx].toDate) }}
+            </span>
+          </v-input>
+          <v-input label="Precio/día: ">
+            {{ ratePlan[idx].price }}€
+          </v-input>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn @click="$router.push({ path:`/booking`, query:{id: center._id, rate: ratePlan[idx]._id}, component:'BookingPage'})">
+            Reservar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
   </v-row>
 </template>
 
@@ -55,6 +61,24 @@ export default {
     getRatePlan () {
       this.center = this.$store.state.centers.find(e => e._id === this.$route.query.id)
       this.ratePlan = this.center.ratePlan.filter(e => e.appliesTo === this.$route.query.type)
+    },
+    getType (idx) {
+      switch (this.ratePlan[idx].appliesTo) {
+        case 'fixedDesk':
+          return 'Mesa Fija'
+        case 'hotDesk':
+          return 'Mesa flexible'
+        case 'office':
+          return 'Oficina'
+        case 'meetingRoom':
+          return 'Sala de conferencias'
+      }
+    },
+    formatDate (date) {
+      const month = this.$store.state.monthShortName[new Date(date).getMonth()]
+      const year = new Date(date).getFullYear()
+      const day = new Date(date).getDate()
+      return `${day}, ${month} ${year}`
     }
   }
 }
