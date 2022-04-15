2c5d733056b1131147a5427f9bcd09c70ae54822
Yanira Alemán <yanira.ar@gmail.com>
2022-04-15 15:29:15 +0100

    MyCenters: responsive adjustments

--- airWorkSpace/pages/host/center/index.vue
+++ airWorkSpace/pages/host/center/index.vue
@@ -2,7 +2,7 @@
   <v-row>
     <v-col v-if="$vuetify.breakpoint.xs">
       <v-card flat>
-        <v-card-title class="justify-center">
+        <v-card-title width="90%" class="px-10 font-weight-bold headline">
           My Centers
         </v-card-title>
       </v-card>
@@ -12,12 +12,13 @@
       <v-card
         v-for="(center, idx) in centers"
         :key="idx"
-        class="mx-auto my-2"
+        class="mx-auto my-2 my-2 rounded-xl"
         width="90%"
+        flat
       >
-        <v-list-item three-line>
+        <v-list-item three-line class="pr-0">
           <v-list-item-content>
-            <v-list-item-title class="text-h7 mb-1 text-wrap">
+            <v-list-item-title class="text-h6 mb-1 text-wrap ">
               {{ center.name }}
             </v-list-item-title>
             <v-list-item-subtitle>{{ center.city }}</v-list-item-subtitle>
@@ -25,7 +26,8 @@
 
           <v-list-item-avatar
             tile
-            size="80"
+            size="130"
+            class="rounded-xl "
             color="grey"
             @click="getOneCenter(center._id)"
           >
@@ -87,7 +89,7 @@
                   <v-col> <a @click="$router.push({ path:`/host/center/bookings/total`, query:{id: center._id}, component:'TotalBookings'})">{{ center.bookings.length }}</a></v-col>
                 </v-row>
               </v-card-subtitle>
-              <v-divider />
+              <!-- <v-divider />
               <v-card-subtitle class="pt-3">
                 <v-row>
                   <v-col cols="8" offset="1" class="py-2">
@@ -98,16 +100,17 @@
                   </v-col>
                   <v-col>€</v-col>
                 </v-row>
-              </v-card-subtitle>
+              </v-card-subtitle> -->
             </v-card-text>
           </div>
         </v-expand-transition>
+        <v-divider />
       </v-card>
     </v-col>
     <!-- Breakpoint SM -->
     <v-col v-if="$vuetify.breakpoint.sm" sm="12">
       <v-card flat>
-        <v-card-title class="justify-center">
+        <v-card-title width="90%" class="px-10 font-weight-bold headline">
           My Centers
         </v-card-title>
       </v-card>
@@ -119,20 +122,28 @@
           <v-card
             v-for="(center, idx) in centers"
             :key="idx"
-            class="mx-auto my-2"
+            class="mx-auto my-2 rounded-xl"
             width="90%"
           >
             <v-list-item three-line>
               <v-list-item-content>
-                <v-list-item-title class="text-h7 mb-1 text-wrap">
+                <v-list-item-title class="text-h6 mb-1 text-wrap">
                   {{ center.name }}
                 </v-list-item-title>
-                <v-list-item-subtitle>{{ center.city }}</v-list-item-subtitle>
+                <div>
+                  <v-chip outlined class="caption">
+                    {{ center.type }}
+                  </v-chip>
+                </div>
+                <v-list-item-subtitle class="mx-2">
+                  {{ center.city }}
+                </v-list-item-subtitle>
               </v-list-item-content>
 
               <v-list-item-avatar
                 tile
-                size="80"
+                size="150"
+                class="rounded-xl"
                 color="grey"
                 @click="getOneCenter(center._id)"
               >
@@ -213,6 +224,97 @@
         </v-col>
       </v-col>
     </v-col>
+    <!-- Breakpoint mdAndUp -->
+    <v-col v-if="$vuetify.breakpoint.mdAndUp" sm="12">
+      <v-card flat>
+        <v-card-title width="85%" class="px-16 mx-12 font-weight-bold headline">
+          My Centers
+        </v-card-title>
+      </v-card>
+      <v-container>
+        <v-card v-if="centers.length === 0">
+          <v-card-text>You don't have any center submited yet.</v-card-text>
+        </v-card>
+        <v-card
+          v-for="(center, idx) in centers"
+          :key="idx"
+          class="mx-auto mb-10 rounded-xl"
+          width="90%"
+          flat
+        >
+          <v-row>
+            <v-col cols="5" lg="6">
+              <v-list-item three-line>
+                <v-list-item-avatar
+                  tile
+                  size="200"
+                  class="rounded-xl"
+                  color="grey"
+                  @click="getOneCenter(center._id)"
+                >
+                  <v-img :src="center.photos[0]" />
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title class="headline font-weight-bold mb-1 text-wrap" :class="$vuetify.breakpoint.md ? 'title':''">
+                    {{ center.name }}
+                  </v-list-item-title>
+                  <v-list-item-subtitle v-if="!$vuetify.breakpoint.md" class="subtitle text-wrap">
+                    <v-icon>mdi-map-marker-outline</v-icon>
+                    {{ center.address1 }} {{ center.address2 ? ` - ${center.address2} - ` : '' }} , {{ center.postalCode }} {{ center.city }} , {{ center.country }}
+                  </v-list-item-subtitle>
+                  <v-list-item-title class="title">
+                    <v-chip outlined>
+                      {{ center.type }}
+                    </v-chip>
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+            <v-col cols="7" lg="6" class="colstyle">
+              <v-card-text class="my-10">
+                <v-card-subtitle class="subtitle-1 py-0">
+                  <v-row>
+                    <v-col cols="6" offset="1" class="py-2">
+                      <v-icon class="warning--text">
+                        mdi-clock
+                      </v-icon>
+                      Bookings to manage:
+                    </v-col>
+                    <v-col>
+                      <a @click="getOneCenterBookings(center._id)">{{ center.bookings.filter(e => e.status === 'open').length }} </a>
+                    </v-col>
+                  </v-row>
+                </v-card-subtitle>
+                <v-card-subtitle class="subtitle-1 py-0">
+                  <v-row>
+                    <v-col cols="6" offset="1" class="py-2">
+                      <v-icon class="success--text">
+                        mdi-check
+                      </v-icon>
+                      Confirmed bookings:
+                    </v-col>
+                    <v-col>
+                      <a @click="$router.push({ path:`/host/center/bookings/confirmed`, query:{id: center._id}, component:'ConfirmedBookings'})">{{ center.bookings.filter(e => e.status === 'confirmed').length }}</a>
+                    </v-col>
+                  </v-row>
+                </v-card-subtitle>
+                <v-card-subtitle class="subtitle-1 py-0">
+                  <v-row>
+                    <v-col cols="6" offset="1" class="py-2">
+                      <v-icon>
+                        mdi-book-outline
+                      </v-icon>
+                      Total Bookings:
+                    </v-col>
+                    <v-col> <a @click="$router.push({ path:`/host/center/bookings/total`, query:{id: center._id}, component:'TotalBookings'})">{{ center.bookings.length }}</a></v-col>
+                  </v-row>
+                </v-card-subtitle>
+              </v-card-text>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-container>
+    </v-col>
   </v-row>
 </template>
 
@@ -250,5 +352,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.colstyle {
+  border-left:dotted 1px grey
+}
 </style>
