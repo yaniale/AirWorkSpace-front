44b5226069caa569f844d9908b5b42aeab9c8d68
Yanira Alemán <yanira.ar@gmail.com>
2022-04-18 21:00:44 +0100

    baseURL changed back to heroku

--- airWorkSpace/nuxt.config.js
+++ airWorkSpace/nuxt.config.js
@@ -77,8 +77,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3000/api'
-    // baseURL: 'https://airworkspace-api.herokuapp.com/api'
+    // baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://airworkspace-api.herokuapp.com/api'
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
